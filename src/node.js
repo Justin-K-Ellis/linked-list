03bd271fdbe46b798d0330281116c6a838903e38
Justin-K-Ellis <jlke45@gmail.com>
2025-01-29 11:23:18 +0900

    Create basic Node class

---
+++ src/node.js
@@ -0,0 +1,9 @@
+class Node {
+  constructor(address = null, value = null, nextNode = null) {
+    this.address = address;
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+export default Node;
